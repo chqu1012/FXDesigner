96cda64d3f4ba7ccb34ed4a6e88987ba39b385c0
Dim <cqd2202@yahoo.de>
2019-11-19 23:14:40 +0100

    FXDES.00007: Added demo class

---
+++ de.dc.fx.ui.renderer.model.ui/resources/de/dc/fx/ui/renderer/model/ui/TableView001.java
@@ -0,0 +1,26 @@
+package de.dc.fx.ui.renderer.model.ui;
+
+import de.dc.fx.ui.renderer.model.FXRoot;
+import de.dc.fx.ui.renderer.model.file.FXRootFile;
+import de.dc.fx.ui.renderer.model.renderer.UIRenderer;
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TableView001 extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		UIRenderer renderer = new UIRenderer();
+		FXRootFile file = new FXRootFile();
+		FXRoot fxRoot = file.load("./resources/001_TableView.ui");
+		Parent root = (Parent) renderer.doSwitch(fxRoot);
+		primaryStage.setScene(new Scene(root));
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
