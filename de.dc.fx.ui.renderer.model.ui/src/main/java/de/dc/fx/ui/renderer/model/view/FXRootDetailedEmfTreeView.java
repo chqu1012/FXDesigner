ad97219df9a406263eea4000557586f06e8c8f36
Dim <cqd2202@yahoo.de>
2019-11-19 22:54:32 +0100

    FXDES.00006: Support remove child tableview

--- de.dc.fx.ui.renderer.model.ui/src/main/java/de/dc/fx/ui/renderer/model/view/FXRootDetailedEmfTreeView.java
+++ de.dc.fx.ui.renderer.model.ui/src/main/java/de/dc/fx/ui/renderer/model/view/FXRootDetailedEmfTreeView.java
@@ -1,5 +1,7 @@
 package de.dc.fx.ui.renderer.model.view;
 
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EObject;
 import org.greenrobot.eventbus.EventBus;
 
@@ -8,10 +10,15 @@ import de.dc.javafx.xcore.workbench.emf.ui.EmfDetailedTreeView;
 import de.dc.javafx.xcore.workbench.emf.ui.EmfTreeModelView;
 import de.dc.javafx.xcore.workbench.emf.ui.event.EventContext;
 import javafx.beans.value.ObservableValue;
+import javafx.scene.control.Button;
 import javafx.scene.control.TreeItem;
 
 public class FXRootDetailedEmfTreeView extends EmfDetailedTreeView<FXRoot> {
 
+	public FXRootDetailedEmfTreeView() {
+		removeChildPropertiesTableView();
+	}
+	
 	@Override
 	protected EmfTreeModelView<FXRoot> initEmfModelTreeView() {
 		return new FXRootEmfTreeModelView();
@@ -29,6 +36,19 @@ public class FXRootDetailedEmfTreeView extends EmfDetailedTreeView<FXRoot> {
 				EventBus.getDefault().post(new EventContext<EObject>("/update/emfworkbench/toolbar", eObject));
 			}
 		}
+	}
+	
+	@Override
+	public boolean initAttributeFormular(EObject eObject) {
+		EList<EAttribute> attributes = eObject.eClass().getEAllAttributes();
+		attributes.forEach(e->{
+			typeFormSwitch.setCurrentEObject(eObject);
+			attributeContainer.getChildren().add(typeFormSwitch.doSwitch(e));
+		});
+		
+		Button acceptAllButton = new Button("Accept All");
+		acceptAllButton.setOnAction(e-> typeFormSwitch.acceptAllValues());
+		return attributeContainer.getChildren().add(acceptAllButton);
 	}
 
 }
