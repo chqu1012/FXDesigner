50104cab9e037aa9b8f7be8e74f4caf2961aa80b
Dim <cqd2202@yahoo.de>
2019-11-23 12:16:49 +0100

    FXDES-00032: Added Accordion Application

---
+++ de.dc.fx.ui.renderer.model.ui/resources/de/dc/fx/ui/renderer/model/ui/AccordionApplication.java
@@ -0,0 +1,14 @@
+package de.dc.fx.ui.renderer.model.ui;
+
+public class AccordionApplication extends UIApplication{
+
+	@Override
+	protected String ui() {
+		return "006_Accordian.ui";
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
